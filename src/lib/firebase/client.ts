94655c6f265dddb4a707d440b7d046f8a8b2d5a2
singh.agrannya <singh.agrannya@gmail.com>
2025-11-02 10:03:20 +0000

    getAuth updated

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -1,4 +1,4 @@
-import { initializeApp, getApps } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
@@ -12,10 +12,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-let app;
-if (!getApps().length) {
-  app = initializeApp(firebaseConfig);
-}
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 const db = getFirestore(app);
 const auth = getAuth(app);
